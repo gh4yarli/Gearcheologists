40dd7e28487000cc57265e2caf1b2306da4b89b0
shatvik-a <shatvikarunkumar@gmail.com>
2025-12-17 20:20:57 -0600

    Adjusted Power Based on AprilTag distance and tested Shooting

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/M3_CommonFunctions.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/M3_CommonFunctions.java
@@ -6,18 +6,21 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.PIDFCoefficients;
+import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-import org.firstinspires.ftc.robotcore.external.hardware.camera.BuiltinCameraDirection;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.controls.ExposureControl;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.controls.GainControl;
+import org.firstinspires.ftc.teamcode.meet1.AprilTag;
 import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
 import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
-
+@SuppressWarnings({"unused", "FieldCanBeLocal", "ParameterCanBeLocal"})
 public abstract class M3_CommonFunctions extends LinearOpMode {
 
     /**
@@ -39,6 +42,18 @@ public abstract class M3_CommonFunctions extends LinearOpMode {
         rightLauncher.setDirection(DcMotorSimple.Direction.REVERSE);
         leftLauncher.setVelocity(launcherVel);
         rightLauncher.setVelocity(-launcherVel);
+    }
+    public void startLaunchers(DcMotorEx launcher, double launcherVel) {
+
+        launcher.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+        launcher.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+
+        PIDFCoefficients pid_right_new = new PIDFCoefficients(50,0.75,1.0,12.7);
+
+        launcher.setPIDFCoefficients(DcMotorEx.RunMode.RUN_USING_ENCODER, pid_right_new);
+
+        launcher.setDirection(DcMotorSimple.Direction.REVERSE);
+        launcher.setVelocity(launcherVel);
     }
 
     /**
@@ -92,7 +107,7 @@ public abstract class M3_CommonFunctions extends LinearOpMode {
      */
     public void startIntake(DcMotor intake1, DcMotor intake2) {
         intake1.setPower(1);
-        intake2.setPower(-1);
+        //intake2.setPower(-1);
     }
 
     /**
@@ -144,15 +159,40 @@ public abstract class M3_CommonFunctions extends LinearOpMode {
         leftLauncher.setDirection(LeftLauncherDirection);
         rightLauncher.setDirection(RightLauncherDirection);
     }
-    public void shootArtifacts(DcMotorEx launcher, DcMotor intake1, DcMotor intake2, double launcherVel) {
+    public void shootArtifacts(DcMotorEx launcher, DcMotor intake1, DcMotor intake2, Servo arm, double launcherVel) {
 
-        launcher.setDirection(DcMotorSimple.Direction.REVERSE);
-
+        launcher.setDirection(DcMotorEx.Direction.REVERSE);
+        launcher.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+        //launcher.setVelocity(launcherVel);
         PIDFCoefficients pid_right_new = new PIDFCoefficients(50,0.75,1.0,12.7);
 
-        launcher.setPIDFCoefficients(DcMotor.RunMode.RUN_USING_ENCODER, pid_right_new);
+        launcher.setPIDFCoefficients(DcMotorEx.RunMode.RUN_USING_ENCODER, pid_right_new);
+
         ElapsedTime runtime = new ElapsedTime();
-        while (runtime.seconds() < 4) {
+        while (runtime.seconds() < 3) {
+            boolean launcherAtSpeed = Math.abs(launcher.getVelocity()) >= launcherVel - 50 && Math.abs(launcher.getVelocity()) <= launcherVel + 50;
+
+            if (launcherAtSpeed) {
+                //startIntake(intake1, intake2);
+                arm.setPosition(0);
+                intake2.setPower(-1);
+            }
+            telemetry.addData("motor velocity", Math.abs(launcher.getVelocity()));
+            telemetry.update();
+        }
+        arm.setPosition(1);
+    }
+    public void shootArtifacts(DcMotorEx launcher, DcMotor intake1, DcMotor intake2, double launcherVel) {
+
+        launcher.setDirection(DcMotorEx.Direction.REVERSE);
+        launcher.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+
+        PIDFCoefficients pid_right_new = new PIDFCoefficients(100,1.5,3.0,10);
+
+        launcher.setPIDFCoefficients(DcMotorEx.RunMode.RUN_USING_ENCODER, pid_right_new);
+
+        ElapsedTime runtime = new ElapsedTime();
+        while (runtime.seconds() < 10) {
             launcher.setVelocity(-launcherVel);
 
             boolean launcherAtSpeed = Math.abs(launcher.getVelocity()) >= launcherVel - 50 && Math.abs(launcher.getVelocity()) <= launcherVel + 50;
@@ -163,8 +203,102 @@ public abstract class M3_CommonFunctions extends LinearOpMode {
             telemetry.addData("motor velocity", Math.abs(launcher.getVelocity()));
             telemetry.update();
         }
-        launcher.setVelocity(0);
         intake2.setPower(0);
     }
+    public void shootBallAprilTagDistance(DcMotorEx launcher, DcMotor intake1, DcMotor intake2, Servo arm, AprilTagProcessor aprilTag, double range) {
+        //double launcherVel = 828.52473 * Math.pow(1.00875, range) + 50;
+        List<AprilTagDetection> currentDetections = aprilTag.getDetections();
 
+        AprilTagDetection desiredTag = detectAprilTag(24, currentDetections);
+        if (desiredTag.id != 24) {
+            return;
+        }
+        range = desiredTag.ftcPose.range;
+
+        double launcherVel = 973.7734 * Math.pow(1.00616, range) + 50;
+        if (range > 90) {
+            launcherVel -= 100;
+        }
+        launcher.setVelocity(launcherVel);
+        //startIntake(intake1, intake2);
+        shootArtifacts(launcher, intake1, intake2, arm, launcherVel);
+
+    }
+    public AprilTagDetection detectAprilTag (int tag, List<AprilTagDetection> currentDetections ){
+
+        // Step through the list of detected tags and look for a matching tag
+        AprilTagDetection dummyTag = new AprilTagDetection(-1, -1 , 1.900F, null, null, null, null, null, null, 123);
+
+        for (AprilTagDetection detection : currentDetections) {
+            // Look to see if we have size info on this tag.
+            if (detection.metadata != null) {
+                //  Check to see if we want to track towards this tag.
+                if ((detection.id == tag)) {
+                    // Yes, we want to use this tag.
+                    return detection;
+                } else {
+                    // This tag is in the library, but we do not want to track it right now.
+                    telemetry.addData("Skipping", "Tag ID %d is not desired", detection.id);
+                }
+            } else {
+                // This tag is NOT in the library, so we don't have enough information to track to it.
+                telemetry.addData("Unknown", "Tag ID %d is not in TagLibrary", detection.id);
+            }
+        }
+        return dummyTag;
+    }
+    public void initAprilTag(AprilTagProcessor aprilTag, VisionPortal visionPortal) {
+        // Create the AprilTag processor by using a builder.
+        aprilTag = new AprilTagProcessor.Builder().build();
+
+        // Adjust Image Decimation to trade-off detection-range for detection-rate.
+        // e.g. Some typical detection data using a Logitech C920 WebCam
+        // Decimation = 1 ..  Detect 2" Tag from 10 feet away at 10 Frames per second
+        // Decimation = 2 ..  Detect 2" Tag from 6  feet away at 22 Frames per second
+        // Decimation = 3 ..  Detect 2" Tag from 4  feet away at 30 Frames Per Second
+        // Decimation = 3 ..  Detect 5" Tag from 10 feet away at 30 Frames Per Second
+        // Note: Decimation can be changed on-the-fly to adapt during a match.
+        aprilTag.setDecimation(2);
+
+        // Create the vision portal by using a builder.
+        visionPortal = new VisionPortal.Builder()
+                .setCamera(hardwareMap.get(WebcamName.class, "Webcam 1"))
+                .addProcessor(aprilTag)
+                .build();
+    }
+
+    public void setManualExposure(VisionPortal visionPortal) {
+        // Wait for the camera to be open, then use the controls
+
+        if (visionPortal == null) {
+            return;
+        }
+
+        // Make sure camera is streaming before we try to set the exposure controls
+        if (visionPortal.getCameraState() != VisionPortal.CameraState.STREAMING) {
+            telemetry.addData("Camera", "Waiting");
+            telemetry.update();
+            while (!isStopRequested() && (visionPortal.getCameraState() != VisionPortal.CameraState.STREAMING)) {
+                sleep(20);
+            }
+            telemetry.addData("Camera", "Ready");
+            telemetry.update();
+        }
+
+        // Set camera controls unless we are stopping.
+        if (!isStopRequested())
+        {
+            ExposureControl exposureControl = visionPortal.getCameraControl(ExposureControl.class);
+            if (exposureControl.getMode() != ExposureControl.Mode.Manual) {
+                exposureControl.setMode(ExposureControl.Mode.Manual);
+                sleep(50);
+            }
+            exposureControl.setExposure(6, TimeUnit.MILLISECONDS);
+            sleep(20);
+            GainControl gainControl = visionPortal.getCameraControl(GainControl.class);
+            gainControl.setGain(250);
+            sleep(20);
+        }
+    }
+    
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/M3_ShootingTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/M3_ShootingTest.java
@@ -1,22 +1,20 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.acmerobotics.dashboard.config.Config;
+
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.PIDFCoefficients;
+import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
-import org.firstinspires.ftc.robotcore.external.hardware.camera.controls.ExposureControl;
-import org.firstinspires.ftc.robotcore.external.hardware.camera.controls.GainControl;
 import org.firstinspires.ftc.vision.VisionPortal;
 import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
 import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
 
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 @TeleOp
 @SuppressWarnings({"unused", "FieldCanBeLocal"})
@@ -24,19 +22,18 @@ import java.util.concurrent.TimeUnit;
 public final class M3_ShootingTest extends M3_CommonFunctions {
     private DcMotorEx launcher;
     DcMotor intake1, intake2;
-    CRServo leftFeeder, rightFeeder;
     VisionPortal visionPortal;
-
-    boolean leftMotorAtSpeed;
+    Servo arm;
     boolean launcherAtSpeed;
+
     public static class Params {
         public double launcherVel = 1700;
 
     }
+
     AprilTagProcessor aprilTag;              // Used for managing the AprilTag detection process.
 
     public static Params PARAMS = new Params();
-
 
 
     @Override
@@ -45,13 +42,12 @@ public final class M3_ShootingTest extends M3_CommonFunctions {
 
         intake1 = hardwareMap.get(DcMotor.class, ConfigurationConstants.Names.FIRST_INTAKE_MOTOR);
         intake2 = hardwareMap.get(DcMotor.class, ConfigurationConstants.Names.SECOND_INTAKE_MOTOR);
-
-        leftFeeder = hardwareMap.get(CRServo.class, "leftFeeder");
-        rightFeeder = hardwareMap.get(CRServo.class, "rightFeeder");
+        arm = hardwareMap.get(Servo.class, ConfigurationConstants.Names.ARM_SERVO);
 
         launcher.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        arm.scaleRange(0.5, 1.0);
         initAprilTag();
-        setManualExposure();
+        setManualExposure(visionPortal);
 
         // 1450 for big triangle, 1800 for small triangle
         double rightLauncherPower = 0.35;
@@ -62,24 +58,26 @@ public final class M3_ShootingTest extends M3_CommonFunctions {
         launcher.setDirection(DcMotorSimple.Direction.REVERSE);
 
         PIDFCoefficients pid_right = launcher.getPIDFCoefficients(DcMotor.RunMode.RUN_USING_ENCODER);
-        PIDFCoefficients pid_left_new = new PIDFCoefficients(50,0.75,1.0,12.5);
-        PIDFCoefficients pid_right_new = new PIDFCoefficients(50,0.75,1.0,12.7);
+        PIDFCoefficients pid_left_new = new PIDFCoefficients(50, 0.75, 1.0, 12.5);
+        PIDFCoefficients pid_right_new = new PIDFCoefficients(50, 0.75, 1.0, 12.7);
 
         launcher.setPIDFCoefficients(DcMotor.RunMode.RUN_USING_ENCODER, pid_right_new);
 
         waitForStart();
         while (opModeIsActive()) {
-            double rangeError = 5000;
-            shootBallDelayedStart(launcher, intake1, intake2, rangeError);
+            intake1.setPower(1);
+            double range;
             int tagFound = 0;
             AprilTagDetection desiredTag;
             List<AprilTagDetection> currentDetections = aprilTag.getDetections();
             desiredTag = detectAprilTag(24, currentDetections);
             if (desiredTag.id == 24) {
                 tagFound = 1;
+                range = desiredTag.ftcPose.range;
                 telemetry.addData("Found", "ID %d (%s), Range %5.1f inches, Bearing %3.0f degrees,  Yaw %3.0f degrees", desiredTag.id, desiredTag.metadata.name, desiredTag.ftcPose.range, desiredTag.ftcPose.bearing, desiredTag.ftcPose.yaw);
-                telemetry.addData("range error inside", rangeError);
+                telemetry.addData("range error inside", range);
                 telemetry.update();
+                shootBallAprilTagDistance(launcher, intake1, intake2, arm, aprilTag, range);
             } else {
                 telemetry.addData("Tag Not Found, ID %d (%s)", desiredTag.id);
                 telemetry.update();
@@ -88,48 +86,17 @@ public final class M3_ShootingTest extends M3_CommonFunctions {
                 telemetry.addData("Tag Not Found, ID %d (%s) and Rotating", desiredTag.id);
                 telemetry.update();
             }
+            telemetry.addData("motor velocity", Math.abs(launcher.getVelocity()));
+            telemetry.update();
         }
     }
-    public void shootBallInstantStart(double launcherVel){
+
+    public void shootBallInstantStart(double launcherVel) {
         launcher.setVelocity(-launcherVel);
         startIntake(intake1, intake2);
 
-        telemetry.addData("right motor velocity", Math.abs(launcher.getVelocity()));
+        telemetry.addData("motor velocity", Math.abs(launcher.getVelocity()));
         telemetry.update();
-    }
-    public void shootBallDelayedStart(DcMotorEx launcher, DcMotor intake1, DcMotor intake2, double rangeError) {
-        double launcherVel = 828.52473 * Math.pow(1.00875, rangeError);
-        launcher.setVelocity(-launcherVel);
-
-        boolean launcherAtSpeed = Math.abs(launcher.getVelocity()) >= launcherVel - 50 && Math.abs(launcher.getVelocity()) <= launcherVel + 50;
-
-        if (launcherAtSpeed) {
-            startIntake(intake1, intake2);
-        }
-    }
-    public AprilTagDetection detectAprilTag (int tag, List<AprilTagDetection> currentDetections ){
-
-        // Step through the list of detected tags and look for a matching tag
-
-        AprilTagDetection dummyTag = new AprilTagDetection(-1, -1 , 1.900F, null, null, null, null, null, null, 123);
-
-        for (AprilTagDetection detection : currentDetections) {
-            // Look to see if we have size info on this tag.
-            if (detection.metadata != null) {
-                //  Check to see if we want to track towards this tag.
-                if ((detection.id == tag)) {
-                    // Yes, we want to use this tag.
-                    return detection;
-                } else {
-                    // This tag is in the library, but we do not want to track it right now.
-                    telemetry.addData("Skipping", "Tag ID %d is not desired", detection.id);
-                }
-            } else {
-                // This tag is NOT in the library, so we don't have enough information to track to it.
-                telemetry.addData("Unknown", "Tag ID %d is not in TagLibrary", detection.id);
-            }
-        }
-        return dummyTag;
     }
     private void initAprilTag() {
         // Create the AprilTag processor by using a builder.
@@ -149,40 +116,6 @@ public final class M3_ShootingTest extends M3_CommonFunctions {
                 .setCamera(hardwareMap.get(WebcamName.class, "Webcam 1"))
                 .addProcessor(aprilTag)
                 .build();
-    }
-
-    private void setManualExposure() {
-        // Wait for the camera to be open, then use the controls
-
-        if (visionPortal == null) {
-            return;
-        }
-
-        // Make sure camera is streaming before we try to set the exposure controls
-        if (visionPortal.getCameraState() != VisionPortal.CameraState.STREAMING) {
-            telemetry.addData("Camera", "Waiting");
-            telemetry.update();
-            while (!isStopRequested() && (visionPortal.getCameraState() != VisionPortal.CameraState.STREAMING)) {
-                sleep(20);
-            }
-            telemetry.addData("Camera", "Ready");
-            telemetry.update();
-        }
-
-        // Set camera controls unless we are stopping.
-        if (!isStopRequested())
-        {
-            ExposureControl exposureControl = visionPortal.getCameraControl(ExposureControl.class);
-            if (exposureControl.getMode() != ExposureControl.Mode.Manual) {
-                exposureControl.setMode(ExposureControl.Mode.Manual);
-                sleep(50);
-            }
-            exposureControl.setExposure(6, TimeUnit.MILLISECONDS);
-            sleep(20);
-            GainControl gainControl = visionPortal.getCameraControl(GainControl.class);
-            gainControl.setGain(250);
-            sleep(20);
-        }
     }
 }
 
