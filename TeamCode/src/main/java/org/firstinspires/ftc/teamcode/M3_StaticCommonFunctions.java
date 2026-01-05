4e7aecba1896b877daca77be008f167e35ea179c
shatvik-a <shatvikarunkumar@gmail.com>
2026-01-05 14:02:04 -0600

    TeleOp Code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/M3_StaticCommonFunctions.java
@@ -0,0 +1,341 @@
+package org.firstinspires.ftc.teamcode;
+
+import android.graphics.Path;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.PIDFCoefficients;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.controls.ExposureControl;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.controls.GainControl;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+@SuppressWarnings({"unused", "FieldCanBeLocal", "ParameterCanBeLocal"})
+public class M3_StaticCommonFunctions {
+    volatile static boolean stopRequested = false;
+
+    /**
+     * Starts the launchers
+     * @param leftLauncher
+     * left launcher motor
+     * @param rightLauncher
+     * right launcher motor
+     * @param launcherVel
+     * power to shoot balls
+     */
+    public static void startLaunchers(DcMotorEx leftLauncher, DcMotorEx rightLauncher, double launcherVel, OpMode opMode) {
+        leftLauncher.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightLauncher.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        leftLauncher.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightLauncher.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        leftLauncher.setDirection(DcMotorSimple.Direction.REVERSE);
+        rightLauncher.setDirection(DcMotorSimple.Direction.REVERSE);
+        leftLauncher.setVelocity(launcherVel);
+        rightLauncher.setVelocity(-launcherVel);
+    }
+    public static void startLaunchers(DcMotorEx launcher, double launcherVel, OpMode opMode) {
+
+        launcher.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+        //launcher.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+
+        PIDFCoefficients pid_right_new = new PIDFCoefficients(50,0.75,1.0,12.7);
+
+        launcher.setPIDFCoefficients(DcMotorEx.RunMode.RUN_USING_ENCODER, pid_right_new);
+        launcher.setDirection(DcMotorSimple.Direction.REVERSE);
+        launcher.setVelocity(launcherVel);
+    }
+
+    /**
+     * Stops the launchers
+     * @param leftLauncher
+     * left launcher motor
+     * @param rightLauncher
+     * right launcher motor
+     */
+    public static void stopLaunchers(DcMotorEx leftLauncher, DcMotorEx rightLauncher) {
+        // setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE) tries to stop the DcMotor instead of just letting it spin
+        // when a power of 0 is requested
+        leftLauncher.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightLauncher.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        leftLauncher.setVelocity(0);
+        rightLauncher.setVelocity(0);
+    }
+
+    /**
+     * Starts the feeders
+     * @param leftFeeder
+     * left feeder servo
+     * @param rightFeeder
+     * right feeder servo
+     */
+    public static void startFeeder(CRServo leftFeeder, CRServo rightFeeder) {
+        leftFeeder.setDirection(DcMotorSimple.Direction.REVERSE);
+        leftFeeder.setPower(-1);
+        rightFeeder.setPower(-1);
+    }
+
+    /**
+     * Stops the feeders
+     * @param leftFeeder
+     * left feeder servo
+     * @param rightFeeder
+     * right feeder servo
+     */
+    public static void stopFeeder(CRServo leftFeeder, CRServo rightFeeder) {
+        leftFeeder.setDirection(DcMotorSimple.Direction.REVERSE);
+        leftFeeder.setPower(0);
+        rightFeeder.setPower(0);
+    }
+
+    /**
+     * Starts the intake
+     * @param intake1
+     * first intake
+     * @param intake2
+     * second intake
+     */
+    public static void startIntake(DcMotor intake1, DcMotor intake2) {
+        intake1.setPower(1);
+        //intake2.setPower(-1);
+    }
+
+    /**
+     * Stops the intake
+     * @param intake1
+     * first intake
+     * @param intake2
+     * second intake
+     */
+    public static void stopIntake(DcMotor intake1, DcMotor intake2) {
+        intake1.setPower(0);
+        intake2.setPower(0);
+    }
+
+    /**
+     * <p></p>
+     * function to shoot balls
+     * @param leftLauncher
+     * left launcher motor
+     * @param rightLauncher
+     * right launcher motor
+     * @param leftFeeder
+     * left feeder servo
+     * @param rightFeeder
+     * right feeder servo
+     * @param intake1
+     * first intake
+     * @param intake2
+     * second intake
+     */
+
+    public static void shootBalls(DcMotorEx leftLauncher, DcMotorEx rightLauncher, CRServo leftFeeder,
+                                  CRServo rightFeeder, DcMotor intake1, DcMotor intake2) {
+        DcMotorEx.Direction LeftFeederDirection = leftFeeder.getDirection();
+        DcMotorEx.Direction LeftLauncherDirection = leftLauncher.getDirection();
+        DcMotorEx.Direction RightLauncherDirection = rightLauncher.getDirection();
+        sleep(2000);
+        for (byte i = 0; i < 4; i++) {
+            if (i != 0) {
+                startIntake(intake1, intake2);
+            }
+            startFeeder(leftFeeder, rightFeeder);
+            sleep(1000);
+            stopFeeder(leftFeeder, rightFeeder);
+            sleep(1000);
+        }
+        stopLaunchers(leftLauncher, rightLauncher);
+        stopIntake(intake1, intake2);
+        leftFeeder.setDirection(LeftFeederDirection);
+        leftLauncher.setDirection(LeftLauncherDirection);
+        rightLauncher.setDirection(RightLauncherDirection);
+    }
+
+    public static void shootArtifacts(DcMotorEx launcher, DcMotor intake1, DcMotor intake2, Servo arm, double launcherVel, OpMode opMode) {
+
+        launcher.setDirection(DcMotorEx.Direction.REVERSE);
+        launcher.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+        //
+        launcher.setVelocity(launcherVel);
+        PIDFCoefficients pid_right_new = new PIDFCoefficients(50,0.75,1.0,12.7);
+
+        launcher.setPIDFCoefficients(DcMotorEx.RunMode.RUN_USING_ENCODER, pid_right_new);
+
+        ElapsedTime runtime = new ElapsedTime();
+        while (runtime.seconds() < 3) {
+            boolean launcherAtSpeed = Math.abs(launcher.getVelocity()) >= launcherVel - 50 && Math.abs(launcher.getVelocity()) <= launcherVel + 50;
+
+            if (launcherAtSpeed) {
+                startIntake(intake1, intake2);
+                arm.setPosition(0);
+                sleep(600);
+                intake2.setPower(-1);
+                intake1.setPower(1);
+
+            }
+            opMode.telemetry.addData("motor velocity", Math.abs(launcher.getVelocity()));
+            opMode.telemetry.update();
+        }
+        //arm.setPosition(1);
+    }
+    public static void shootArtifacts(DcMotorEx launcher, DcMotor intake1, DcMotor intake2, double launcherVel, OpMode opMode) {
+
+        launcher.setDirection(DcMotorEx.Direction.REVERSE);
+        launcher.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+
+        PIDFCoefficients pid_right_new = new PIDFCoefficients(100,1.5,3.0,10);
+
+        launcher.setPIDFCoefficients(DcMotorEx.RunMode.RUN_USING_ENCODER, pid_right_new);
+
+        ElapsedTime runtime = new ElapsedTime();
+        while (runtime.seconds() < 10) {
+            launcher.setVelocity(-launcherVel);
+
+            boolean launcherAtSpeed = Math.abs(launcher.getVelocity()) >= launcherVel - 50 && Math.abs(launcher.getVelocity()) <= launcherVel + 50;
+
+            if (launcherAtSpeed) {
+                startIntake(intake1, intake2);
+            }
+            opMode.telemetry.addData("motor velocity", Math.abs(launcher.getVelocity()));
+            opMode.telemetry.update();
+        }
+        intake2.setPower(0);
+    }
+    public static void shootBallAprilTagDistance(DcMotorEx launcher, DcMotor intake1, DcMotor intake2, Servo arm, AprilTagProcessor aprilTag, OpMode opMode) {
+        //double launcherVel = 828.52473 * Math.pow(1.00875, range) + 50;
+        M3_TeleOp.shootBallsRunning = true;
+        List<AprilTagDetection> currentDetections = aprilTag.getDetections();
+
+        AprilTagDetection desiredTag = detectAprilTag(24, currentDetections, opMode);
+        if (desiredTag.id != 24) {
+            return;
+        }
+        double range = desiredTag.ftcPose.range;
+
+        double launcherVel = 973.7734 * Math.pow(1.00616, range) + 50;
+        if (range > 90) {
+            launcherVel -= 100;
+        }
+        launcher.setVelocity(launcherVel-50);
+        //startIntake(intake1, intake2);
+        shootArtifacts(launcher, intake1, intake2, arm, launcherVel, opMode);
+        M3_TeleOp.shootBallsRunning = false;
+
+    }
+    public static AprilTagDetection detectAprilTag (int tag, List<AprilTagDetection> currentDetections, OpMode opMode ){
+
+        // Step through the list of detected tags and look for a matching tag
+        AprilTagDetection dummyTag = new AprilTagDetection(-1, -1 , 1.900F, null, null, null, null, null, null, 123);
+
+        for (AprilTagDetection detection : currentDetections) {
+            // Look to see if we have size info on this tag.
+            if (detection.metadata != null) {
+                //  Check to see if we want to track towards this tag.
+                if ((detection.id == tag)) {
+                    // Yes, we want to use this tag.
+                    opMode.telemetry.addData("April Tag Detected", detection.id);
+                    return detection;
+                } else {
+                    // This tag is in the library, but we do not want to track it right now.
+                    opMode.telemetry.addData("Skipping", "Tag ID %d is not desired", detection.id);
+                }
+            } else {
+                // This tag is NOT in the library, so we don't have enough information to track to it.
+                opMode.telemetry.addData("Unknown", "Tag ID %d is not in TagLibrary", detection.id);
+            }
+        }
+        return dummyTag;
+    }
+    public static void initAprilTag(AprilTagProcessor aprilTag, VisionPortal visionPortal, OpMode opMode) {
+        // Create the AprilTag processor by using a builder.
+        aprilTag = new AprilTagProcessor.Builder().build();
+
+        // Adjust Image Decimation to trade-off detection-range for detection-rate.
+        // e.g. Some typical detection data using a Logitech C920 WebCam
+        // Decimation = 1 ..  Detect 2" Tag from 10 feet away at 10 Frames per second
+        // Decimation = 2 ..  Detect 2" Tag from 6  feet away at 22 Frames per second
+        // Decimation = 3 ..  Detect 2" Tag from 4  feet away at 30 Frames Per Second
+        // Decimation = 3 ..  Detect 5" Tag from 10 feet away at 30 Frames Per Second
+        // Note: Decimation can be changed on-the-fly to adapt during a match.
+        aprilTag.setDecimation(2);
+
+        // Create the vision portal by using a builder.
+        visionPortal = new VisionPortal.Builder()
+                .setCamera(opMode.hardwareMap.get(WebcamName.class, "Webcam 1"))
+                .addProcessor(aprilTag)
+                .build();
+    }
+    public static void setManualExposure(VisionPortal visionPortal, OpMode opMode) {
+        // Wait for the camera to be open, then use the controls
+
+        if (visionPortal == null) {
+            return;
+        }
+
+        // Make sure camera is streaming before we try to set the exposure controls
+        if (visionPortal.getCameraState() != VisionPortal.CameraState.STREAMING) {
+            opMode.telemetry.addData("Camera", "Waiting");
+            opMode.telemetry.update();
+            while (!isStopRequested() && (visionPortal.getCameraState() != VisionPortal.CameraState.STREAMING)) {
+                sleep(20);
+            }
+            opMode.telemetry.addData("Camera", "Ready");
+            opMode.telemetry.update();
+        }
+
+        // Set camera controls unless we are stopping.
+        if (!isStopRequested())
+        {
+            ExposureControl exposureControl = visionPortal.getCameraControl(ExposureControl.class);
+            if (exposureControl.getMode() != ExposureControl.Mode.Manual) {
+                exposureControl.setMode(ExposureControl.Mode.Manual);
+                sleep(50);
+            }
+            exposureControl.setExposure(6, TimeUnit.MILLISECONDS);
+            sleep(20);
+            GainControl gainControl = visionPortal.getCameraControl(GainControl.class);
+            gainControl.setGain(250);
+            sleep(20);
+        }
+    }
+    /**
+     * Sleeps for the given amount of milliseconds, or until the thread is interrupted (which usually
+     * indicates that the OpMode has been stopped).
+     * <p>This is simple shorthand for {@link Thread#sleep(long) sleep()}, but it does not throw {@link InterruptedException}.</p>
+     *
+     * @param milliseconds amount of time to sleep, in milliseconds
+     * @see Thread#sleep(long)
+     */
+    public static void sleep(long milliseconds) {
+        try {
+            Thread.sleep(milliseconds);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+    /**
+     * Determine whether the OpMode has been asked to stop.
+     *
+     * <p>If this method returns false, you should break out of any loops
+     * and allow the OpMode to exit at its earliest convenience.</p>
+     *
+     * @return Whether the OpMode has been asked to stop
+     */
+    public static boolean isStopRequested() {
+        return stopRequested || Thread.currentThread().isInterrupted();
+    }
+
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/M3_TeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/M3_TeleOp.java
@@ -1,4 +1,271 @@
 package org.firstinspires.ftc.teamcode;
 
-public class M3_TeleOp {
+
+import com.qualcomm.hardware.gobilda.GoBildaPinpointDriver;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.PIDFCoefficients;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.teamcode.meet2.M2_CompTeleop;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+
+import java.util.List;
+
+@TeleOp(name = "M3_CompTeleop", group = "Competition")
+public class M3_TeleOp extends OpMode {
+
+    // Mecanum Wheels
+    DcMotor frontLeftDrive;
+    DcMotor frontRightDrive;
+    DcMotor backLeftDrive;
+    DcMotor backRightDrive;
+
+    // Intake/Launcher
+    DcMotorEx launcher;
+    DcMotor intake1, intake2;
+    Servo armServo;
+    ElapsedTime timer;
+
+    //double forwardBackward;
+    //double strafeRightLeft;
+    //double rotate = 0;
+    AprilTagProcessor aprilTag;
+    VisionPortal visionPortal;
+
+    public static M2_CompTeleop.Params PARAMS = new M2_CompTeleop.Params();
+
+    public GoBildaPinpointDriver pinpoint;
+
+    M3_StaticCommonFunctions scuff = new M3_StaticCommonFunctions();
+
+    boolean toggle = false; //this checks if a has been toggled
+    boolean last = false; //checks the value given the last time a was pressed
+
+    static boolean shootBallsRunning = false;
+
+    @Override
+    public void init() {
+
+        // initializing stuff
+        frontLeftDrive = hardwareMap.get(DcMotor.class, "leftFront");
+        frontRightDrive = hardwareMap.get(DcMotor.class, "rightFront");
+        backLeftDrive = hardwareMap.get(DcMotor.class, "leftBack");
+        backRightDrive = hardwareMap.get(DcMotor.class, "rightBack");
+
+        // Intakes, Servo,  and Launcher
+        launcher = hardwareMap.get(DcMotorEx.class, "launcher");
+        intake1 = hardwareMap.get(DcMotor.class, "intake1");
+        intake2 = hardwareMap.get(DcMotor.class, "intake2");
+        armServo = hardwareMap.get(Servo.class, "armServo");
+
+        initAprilTag();
+        timer = new ElapsedTime();
+
+        // reverse the left motors just because that's how it works
+        frontLeftDrive.setDirection(DcMotor.Direction.REVERSE);
+        backLeftDrive.setDirection(DcMotorSimple.Direction.REVERSE);
+        launcher.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        PIDFCoefficients pidf = new PIDFCoefficients(50, 0.75, 1.0, 12.7);
+
+        launcher.setPIDFCoefficients(DcMotor.RunMode.RUN_USING_ENCODER, pidf);
+
+        // Pinpoint
+        pinpoint = hardwareMap.get(GoBildaPinpointDriver.class, ConfigurationConstants.Names.ODOMETRY_COMPUTER);
+
+        double mmPerTick = PARAMS.inPerTick * 25.4;
+        pinpoint.setEncoderResolution(1 / mmPerTick, DistanceUnit.MM);
+        pinpoint.setOffsets(mmPerTick * PARAMS.parYTicks, mmPerTick * PARAMS.perpXTicks, DistanceUnit.MM);
+
+        //Encoder Directions
+        pinpoint.setEncoderDirections(GoBildaPinpointDriver.EncoderDirection.REVERSED,
+                GoBildaPinpointDriver.EncoderDirection.REVERSED);
+        //double mmPerTick = PARAMS.inPerTick * 25.4;
+        pinpoint.setEncoderResolution(1 / mmPerTick, DistanceUnit.MM);
+        pinpoint.setOffsets(mmPerTick * PARAMS.parYTicks, mmPerTick * PARAMS.perpXTicks, DistanceUnit.MM);
+        //Encoder Directions
+        pinpoint.setEncoderDirections(GoBildaPinpointDriver.EncoderDirection.REVERSED,
+                GoBildaPinpointDriver.EncoderDirection.REVERSED);
+
+        pinpoint.resetPosAndIMU();
+    }
+    @Override
+    public void start(){
+        launcher.setVelocity(1380);
+    }
+
+    @Override
+    public void loop() {
+
+        pinpoint.update();
+
+        boolean currentButton = gamepad1.a; //sets which button i want to toggle
+
+
+        // INTAKE AND LAUNCHER CODE BELOW
+        intake1.setPower(1.0);
+        //intake2.setPower(-1.0);
+
+        //Servo Bind
+
+        if (gamepad2.x) {
+            armServo.setPosition(0);
+        }
+        // if left bumper isn't pressed, intake 2 won't run
+        else {
+            armServo.setPosition(1);
+        }
+        if (gamepad1.a){
+            pinpoint.resetPosAndIMU();
+        }
+
+        /* if (gamepad1.left_bumper) {
+            drive(-gamepad1.left_stick_y, gamepad1.left_stick_x, gamepad1.right_stick_x);
+        } else {
+            driveFieldRelative(-gamepad1.left_stick_y, gamepad1.left_stick_x, gamepad1.right_stick_x);
+        }
+
+         */
+
+        if (gamepad1.b && !last && timer.milliseconds() > 1000) {
+            toggle = !toggle; // this flips the value of the variable to the opposite
+            last = gamepad1.b; //adds current button's value so it can be compared for the next loop
+            timer.reset();
+        }
+
+        // depending on the value (true or false) it will go to either robot centric or field centric
+        if (toggle) {
+            drive(-gamepad1.left_stick_y, gamepad1.left_stick_x, gamepad1.right_stick_x);
+        } else {
+            driveFieldRelative(-gamepad1.left_stick_y, gamepad1.left_stick_x, gamepad1.right_stick_x);
+        }
+
+        if (gamepad2.left_bumper) {
+            intake2.setPower(-1.0);
+        }
+        // if right trigger is pressed, it will launch the artifacts
+        if (gamepad2.right_trigger > 0) {
+
+            //Calling shooter method that should dynamically set the velocity based on Apriltag detection
+            scuff.shootBallAprilTagDistance(launcher, intake1, intake2, armServo, aprilTag, this);
+
+        } else if (gamepad2.right_bumper) {
+            launcher.setVelocity(1800);
+        }
+        if (!shootBallsRunning){
+            intake2.setPower(0);
+        }
+        // Emergency Brake
+        if (gamepad2.b) {
+            launcher.setVelocity(0);
+            intake1.setPower(0);
+            intake2.setPower(0);
+            frontLeftDrive.setPower(0);
+            frontRightDrive.setPower(0);
+            backLeftDrive.setPower(0);
+            backRightDrive.setPower(0);
+            armServo.setPosition(1);
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                // needed this to not have warning
+            }
+        }
+        else {
+            launcher.setVelocity(1300);
+            intake1.setPower(1);
+        }
+    }
+
+    public AprilTagDetection detectAprilTag (int tag, List<AprilTagDetection> currentDetections ){
+
+        // Step through the list of detected tags and look for a matching tag
+        AprilTagDetection dummyTag = new AprilTagDetection(-1, -1 , 1.900F, null, null, null, null, null, null, 123);
+
+        for (AprilTagDetection detection : currentDetections) {
+            // Look to see if we have size info on this tag.
+            if (detection.metadata != null) {
+                //  Check to see if we want to track towards this tag.
+                if ((detection.id == tag)) {
+                    // Yes, we want to use this tag.
+                    telemetry.addData("\n\n\n\n", "TAG FOUND!!!!!!!");
+                    return detection;
+                } else {
+                    // This tag is in the library, but we do not want to track it right now.
+                    telemetry.addData("Skipping", "Tag ID %d is not desired", detection.id);
+                }
+            } else {
+                // This tag is NOT in the library, so we don't have enough information to track to it.
+                telemetry.addData("Unknown", "Tag ID %d is not in TagLibrary", detection.id);
+            }
+        }
+        return dummyTag;
+    }
+
+    public void drive(double forwardBackward, double strafeRightLeft, double rotate) {
+        // This calculates the power needed for each wheel based on the amount of forward,
+        // strafe right, and rotate
+        double frontLeftPower = forwardBackward + strafeRightLeft + rotate;
+        double frontRightPower = forwardBackward - strafeRightLeft - rotate;
+        double backRightPower = forwardBackward + strafeRightLeft - rotate;
+        double backLeftPower = forwardBackward - strafeRightLeft + rotate;
+
+        double maxPower = 1.0;
+        double maxSpeed = 1.0;  // make this slower for outreaches
+
+        // This is needed to make sure we don't pass > 1.0 to any wheel
+        // It allows us to keep all of the motors in proportion to what they should
+        // be and not get clipped
+        maxPower = Math.max(maxPower, Math.abs(frontLeftPower));
+        maxPower = Math.max(maxPower, Math.abs(frontRightPower));
+        maxPower = Math.max(maxPower, Math.abs(backRightPower));
+        maxPower = Math.max(maxPower, Math.abs(backLeftPower));
+
+        // We multiply by maxSpeed so that it can be set lower for outreaches
+        // When a young child is driving the robot, we may not want to allow full
+        // speed.
+        frontLeftDrive.setPower(maxSpeed * (frontLeftPower / maxPower));
+        frontRightDrive.setPower(maxSpeed * (frontRightPower / maxPower));
+        backLeftDrive.setPower(maxSpeed * (backLeftPower / maxPower));
+        backRightDrive.setPower(maxSpeed * (backRightPower / maxPower));
+    }
+    private void driveFieldRelative(double forwardBackward, double strafeRightLeft, double rotate) {
+
+        telemetry.addLine("Hold left bumper to drive in robot relative");
+        telemetry.addLine("The left joystick sets the robot direction");
+        telemetry.addLine("Moving the right joystick left and right turns the robot");
+        // First, convert direction being asked to drive to polar coordinates
+        double theta = Math.atan2(forwardBackward, strafeRightLeft);
+        double r = Math.hypot(strafeRightLeft, forwardBackward);
+        double pinpoint_Heading = pinpoint.getHeading(AngleUnit.RADIANS);
+        // Second, rotate angle by the angle the robot is pointing
+        theta = AngleUnit.normalizeRadians(theta -
+                pinpoint_Heading);
+
+        // Third, convert back to cartesian
+        double newForwardBackward = r * Math.sin(theta);
+        double newStrafeRightLeft = r * Math.cos(theta);
+
+        // Finally, call the drive method with robot relative forward and right amounts
+        drive(newForwardBackward, newStrafeRightLeft, rotate);
+
+    }
+    public void initAprilTag() {
+        aprilTag = new AprilTagProcessor.Builder().build();
+        aprilTag.setDecimation(2);
+
+        visionPortal = new VisionPortal.Builder()
+                .setCamera(hardwareMap.get(WebcamName.class, "Webcam 1"))
+                .addProcessor(aprilTag)
+                .build();
+    }
 }
