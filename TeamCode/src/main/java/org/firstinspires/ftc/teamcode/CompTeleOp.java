60e9d184fab50d0ece65e1c5014b16f3eac65fbb
shatvik-a <shatvikarunkumar@gmail.com>
2026-01-24 17:51:14 -0600

    Revert "TeleOp - Changed the code to improve shooting the artifacts"
    This reverts commit 7d603cdb3f700ebd83f7d7b09ae129acf6c5706d.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CompTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CompTeleOp.java
@@ -70,9 +70,9 @@ public class CompTeleOp extends BaseTeleOp {
             if (atSpeed) {
                 armServo.setPosition(0);
                 if (launchTimer.milliseconds() > 300) {
-                    intake2.setPower(-0.5);
+                    intake1.setPower(1);
                     if (launchTimer.milliseconds() > 500) {
-                        intake1.setPower(1);
+                        intake2.setPower(-0.5);
                     }
                 } else {
                     intake1.setPower(0);
